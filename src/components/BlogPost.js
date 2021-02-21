02af98da1ced12f278dbad83f5683e5446e371b8
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-20 20:00:41 -0500

    commit tree ee77fa7057c10b7ec8b3ff31c8e7c375fa28d7f4

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -12,13 +12,11 @@ function BlogPost() {
       />
       <Comment commentText={"I agree with this statement. - Angela Merkel"} />
       <Comment commentText={"A universal truth. - Noam Chomsky"} />
-      <Comment
-        commentText={
-          "Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"
-        }
-      />
+      <Comment commentText={"Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"} />
     </div>
   );
 }
+
+console.log(BlogPost())
 
 export default BlogPost;
